5d25a708ada5deb0608d35b77f08373b96b03b59
Lemming-CS <220599209+Lemming-CS@users.noreply.github.com>
2025-11-30 19:06:22 +0600

    Solve jsExercises 8-11

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...removeItems) {
+    for (let i = 0; i<arr.length; i++) {
+        for (const item of removeItems) {
+            let itemIndex = arr.indexOf(item);
+            if (itemIndex === -1) {
+                continue;
+            }
+            arr.splice(itemIndex, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,15 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || typeof(a) != "number" || typeof(b) != "number" || a % 1 !== 0 || b % 1 !== 0) {
+        return 'ERROR'
+    }
+    if (a > b) {
+        [b, a] = [a, b];
+    }
+    let sum = 0;
+    for (let i=a; i<=b; i++) {
+        sum +=i;
+    } 
+    return sum;
 
 };
 
